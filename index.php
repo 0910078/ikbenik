7bc0c17842494d2d80177bb0e10b5b8d9a30f6f9
Marvin Joppe <0910078@hr.nl>
2015-12-07 14:11:19 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Praktijk ik ben ik</title>
+    <link href="css/stylesheet.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<div class="container">
+
+    <header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li>Agenda</li>
+            <li>Mijn afspraken</li>
+            <li>Contact</li>
+            <li>Inloggen</li>
+            <li>Registreren</li>
+        </ul>
+    </nav>
+        <div>
+            
+        </div>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+
+</div>
+</body>
+</html>

bce93cdbd6a5b9105bc8017f1fe7e44a32c24aa9
Marvin Joppe <0910078@hr.nl>
2015-12-07 14:18:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -20,16 +20,18 @@
         </ul>
     </nav>
         <div>
-            
+            <img src="http://portfoliotheme.org/enigmatic/wp-content/uploads/sites/9/2012/07/placeholder1.jpg"/>
         </div>
     </header>
 
     <main>
-
+        <h1>Header</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias delectus fugit labore maxime nostrum qui suscipit! Aliquid dicta in inventore necessitatibus, quos rerum vitae? Consectetur modi numquam praesentium sequi veritatis?</p>
     </main>
 
     <footer>
-
+        <h5>Header2</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid, asperiores delectus ea eaque exercitationem harum natus non odio officia quidem quis, voluptas voluptates. A cumque doloribus illo molestiae rerum.</p>
     </footer>
 
 </div>

01dfa5d6775beac0a5cd6414aba09089fc971cb2
Marvin Joppe <0910078@hr.nl>
2015-12-16 11:11:13 +0100

    created contact.php

---
+++ contact.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Praktijk ik ben ik</title>
+    <link href="css/stylesheet.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+<div class="container">
+
+    <header>
+
+        <nav class="navigation">
+            <ul class="navigation">
+                <li class="navigation"><a class="navigation" href="index.php"><b>Home</b></a></li>
+                <li class="navigation"><a class="navigation" href=#>Agenda</a></li>
+                <li class="navigation"><a class="navigation" href=#>Mijn afspraken</a></li>
+                <li class="navigation2"><a class="navigation2" href="contact.php">Contact</a></li>
+                <li class="navigation"><a class="navigation" href=#>Inloggen</a></li>
+                <li class="navigation"><a class="navigation" href="registreren.php">Registreren</a></li>
+            </ul>
+        </nav>
+
+        <div class ="logo">
+            <img class="logo" src="images/logo-ikbenik-metal-brush1.png"/>
+        </div>
+
+    </header>
+
+    <main>
+
+        <div class="welkom">
+            <h1>Contact</h1>
+        </div>
+
+        <div id="map">
+            
+        </div>
+
+    </main>
+
+    <footer>
+        <h5>Footer tekst</h5>
+        <p>&copy;Praktijk ik ben ik 2015</p>
+    </footer>
+
+</div>
+</body>
+</html>
